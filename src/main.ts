551d727c8a6fe10f6bdf53d1831a0377e223f7a4
YukiTominaga <tominaga.yuki@cloud-ace.jp>
2023-10-26 17:06:01 +0900

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,32 @@
-import { MatchingEngine } from 'langchain/vectorstores/googlevertexai';
-import { Document } from 'langchain/document';
-import { SyntheticEmbeddings } from 'langchain/embeddings/fake';
-import { GoogleCloudStorageDocstore } from 'langchain/stores/doc/gcs';
+// node_modules/langchain/dist/vectorstores/googlevertexai.js
+// node_modules/langchain/dist/utils/googlevertexai-connection.js
+// 以上2つのファイルについて、プロジェクトIDを指定している部分をプロジェクト番号に書き換える
 
-const embeddings = new SyntheticEmbeddings({
-  vectorSize: Number.parseInt(process.env.SYNTHETIC_EMBEDDINGS_VECTOR_SIZE ?? '768', 10),
+import { GoogleVertexAIEmbeddings } from 'langchain/embeddings/googlevertexai';
+import { GoogleCloudStorageDocstore } from 'langchain/stores/doc/gcs';
+import { MatchingEngine, MatchingEngineArgs } from 'langchain/vectorstores/googlevertexai';
+
+const embeddings = new GoogleVertexAIEmbeddings({
+  temperature: 0.2,
 });
 
 const store = new GoogleCloudStorageDocstore({
   bucket: process.env.GOOGLE_CLOUD_STORAGE_BUCKET!,
+  prefix: 'card_list/',
 });
 
-const config = {
-  index: process.env.GOOGLE_VERTEXAI_MATCHINGENGINE_INDEX!,
-  indexEndpoint: process.env.GOOGLE_VERTEXAI_MATCHINGENGINE_INDEXENDPOINT!,
-  apiVersion: 'v1beta1',
+const config: MatchingEngineArgs = {
+  location: 'asia-northeast1',
+  endpoint: 'asia-northeast1-aiplatform.googleapis.com',
+  index: process.env.GOOGLE_VERTEXAI_VECTORSTORE_INDEX!,
+  indexEndpoint: process.env.GOOGLE_VERTEXAI_VECTORSTORE_INDEXENDPOINT!,
+  deployedIndexId: process.env.GOOGLE_VERTEXAI_VECTORSTORE_DEPLOYEDINDEXID!,
+  apiVersion: 'v1',
   docstore: store,
 };
 
 const engine = new MatchingEngine(embeddings, config);
+
+const results = await engine.similaritySearch('');
+
+console.log(results);
